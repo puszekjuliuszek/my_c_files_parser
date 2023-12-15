2b35d889e7f5e6f10bf909a4eb6c3726da6dfb8e
Juliusz Wasieleski <tojabonibykto@gmail.com>
2023-12-15 16:13:03 +0100

    cos

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # my_c_files_parser
+## how to use
+run:
+```angular2html
+git clone https://github.com/puszekjuliuszek/my_c_files_parser
+```
+
+give path to your c file and thats all
+
+## the output of parser
+.json file of structure:
+```angular2html
+{
+'array_info': [('A', [4096, 4096])], 
+'loop_bounds': [('i', 0, 4096, 1), ('j', 0, 8, 1), ('k', 0, 4096, 1)], 
+'data_deps': [[['A', 'k', 'i'], ['A', 'k', 'i'], ['A', 'k', 'i'], 90]]
+}
+```
+## license
--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from ast_tree_parser import parse_ast_tree
 from pycparser import parse_file
 
 
-def return_features_json(path: str = './test_dir/test.c') -> dict:
+def return_features_json(path: str = '.\\test_dir\\test.c') -> dict:
     ast_tree = parse_file(path, use_cpp=True, cpp_path='gcc',
                           cpp_args=['-E', r'-ID:\prywatne\jul\pycparser\utils\fake_libc_include',
-                                    r'-ID:\Prywatne\jul\python_projekty\c_files_parser\test_dir'])
+                                    r'-ID:\Prywatne\jul\python_projekty\my_c_files_parser\test_dir'])
     return parse_ast_tree(ast_tree)
 
 
 if __name__ == "__main__":
-    return_features_json()
+    print(return_features_json())
